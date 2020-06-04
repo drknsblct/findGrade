58632af5f2b318fa9fc59f2c27bd7d445d638d35
drknsblct <drknsblct@gmail.com>
2020-06-04 23:11:05 +0300

    Added menu

--- src/MesosOros.java
+++ src/MesosOros.java
@@ -7,17 +7,31 @@ public class MesosOros {
     public static void main(String[] args) {
         ArrayList<Double> list = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
-    while (true) {
-        courses(list);
-        printArray(list);
-        System.out.println("Reset? (y/n)");
-        String answer = scan.nextLine();
-        if(!answer.equals("y")){
-            break;
+        while (true) {
+            System.out.println("1. Add Courses & Find Average Score || 2. View Average Score || 3. Reset List || 4. " +
+                    "Exit");
+            System.out.print("Choice: ");
+            int answer = scan.nextInt();
+            if (answer == 1) {
+                courses(list);
+                printArray(list);
+            }
+            if (answer == 2) {
+                if (list.size() == 0) {
+                    System.out.println("The list is currently empty!\n");
+                    continue;
+                }
+                printArray(list);
+            }
+            if (answer == 3) {
+                System.out.println("Emptied List!\n");
+                list.clear();
+            }
+            if (answer == 4) {
+                System.out.println("Exiting program!");
+                break;
+            }
         }
-    }
-
-
     }
 
     public static double edit(double va9mos, double pososto) {
@@ -28,15 +42,16 @@ public class MesosOros {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter number of courses: ");
         int courses = scan.nextInt();
-        System.out.print("Enter number of ergasies: ");
-        int ergasies = scan.nextInt();
+        System.out.print("Enter number of CW: ");
+        int cw = scan.nextInt();
+        System.out.println();
 
         for (int i = 0; i < courses; i++) {
             double va9mos;
             double pososto;
             double sum = 0.0;
 
-            for (int j = 0; j < ergasies; j++) {
+            for (int j = 0; j < cw; j++) {
                 System.out.printf("Ma9hma[%d] - Ergasia[%d]\n", i + 1, j + 1);
                 System.out.print("Va9mos: ");
                 va9mos = scan.nextDouble();
@@ -46,9 +61,6 @@ public class MesosOros {
                 }
                 System.out.print("Pososto: ");
                 pososto = scan.nextInt();
-                System.out.println();
-
-
                 sum += edit(va9mos, pososto);
             }
             list.add(sum);
@@ -67,7 +79,7 @@ public class MesosOros {
             sum += num;
 
         }
-        System.out.printf("Mesos oros: %.2f\n", sum / list.size());
+        System.out.printf("-->Mesos oros: %.2f<--\n\n", sum / list.size());
     }
 }
 
