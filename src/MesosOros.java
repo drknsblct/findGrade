1f2b09e104c8ca81954c5e67af511d9f547e7421
drknsblct <drknsblct@gmail.com>
2020-06-11 02:34:58 +0300

    Added functionality to find others average score

--- src/MesosOros.java
+++ src/MesosOros.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class MesosOros {
@@ -7,10 +8,22 @@ public class MesosOros {
         ArrayList<Double> list = new ArrayList<>();
         ArrayList<Double> averageList = new ArrayList<>();
         ArrayList<String> coursesList = new ArrayList<>();
+
+        //Find others average score
+        ArrayList<Double> megaList = new ArrayList<>();
+        ArrayList<Double> editedMegaList = new ArrayList<>();
+        ArrayList<Double> list1 = new ArrayList<>();
+        ArrayList<Double> list2 = new ArrayList<>();
+        ArrayList<Double> list3 = new ArrayList<>();
+        ArrayList<Double> list4 = new ArrayList<>();
+        ArrayList<Double> list5 = new ArrayList<>();
+        ArrayList<Double> list6 = new ArrayList<>();
+
+
         Scanner scan = new Scanner(System.in);
         while (true) {
             System.out.println("1. Add Courses || 2. View Average Score || 3. Reset List || 4. " +
-                    "Find Average Score || 5. Delete Entries || 0. Exit");
+                    "Find Average Score || 5. Delete Entries || 6. Find Others Average Score || 0. Exit");
             System.out.print("Choice: ");
             int answer = scan.nextInt();
             if (answer == 0) {
@@ -48,6 +61,10 @@ public class MesosOros {
             if (answer == 5) {
                 System.out.println("<<< Delete Entries >>>");
                 deleteItems(list, coursesList);
+            }
+            if (answer == 6) {
+                System.out.println("<<< Find Others Average Score >>>");
+                findOthersAverageScore(megaList, list1, list2, list3, list4, list5, list6, editedMegaList);
             }
 
         }
@@ -136,7 +153,7 @@ public class MesosOros {
     public static void deleteItems(ArrayList<Double> list, ArrayList<String> coursesList) {
         Scanner scan = new Scanner(System.in);
         while (true) {
-            if (list.isEmpty()){
+            if (list.isEmpty()) {
                 System.out.println("The list is currently empty!\n");
                 break;
             }
@@ -195,11 +212,89 @@ public class MesosOros {
         }
     }
 
-    public static void findFinalTest() {}
+    public static void findFinalTest() {
+    }
 
-    public static void findOthersAverageScore(ArrayList<Double> megaList, ArrayList<Double> list1, ArrayList<Double> list2, ArrayList<Double> list3, ArrayList<Double> list4, ArrayList<Double> list5){
 
+    public static void findOthersAverageScore(ArrayList<Double> megaList, ArrayList<Double> list1,
+                                              ArrayList<Double> list2, ArrayList<Double> list3,
+                                              ArrayList<Double> list4, ArrayList<Double> list5,
+                                              ArrayList<Double> list6, ArrayList<Double> editedMegalist) {
+        ArrayList<String> names = new ArrayList<String>() {
+            {
+                add("Panagiwths");
+                add("Iakwvos");
+                add("Spyros");
+                add("Kelaidiths");
+                add("Ntostoglou");
+                add("Swthrhs");
+                add("Periklhs");
+            }
+
+        };
+        Scanner scan = new Scanner(System.in);
+        for (int i = 1; i <= 42; i++) {
+            System.out.printf("Va9mos[%d]: ", i);
+            double va9mos = Double.parseDouble(scan.nextLine());
+            if (i % 7 == 0) {
+                System.out.println();
+            }
+            if (i <= 7) {
+                list1.add(va9mos);
+            } else if (i <= 14) {
+                list2.add(va9mos);
+            } else if (i <= 21) {
+                list3.add(va9mos);
+            } else if (i <= 28) {
+                list4.add(va9mos);
+            } else if (i <= 35) {
+                list5.add(va9mos);
+            } else {
+                list6.add(va9mos);
+            }
+        }
+
+        for (int i = 0; i < 7; i++) {
+            megaList.add(list1.get(i) + list2.get(i) + list3.get(i) + list4.get(i) + list5.get(i) + list6.get(i));
+        }
+        for (Double num : megaList) {
+            editedMegalist.add(num / 6);
+        }
+
+        double sum = 0.0;
+        for (Double num : editedMegalist) {
+            sum += num;
+
+        }
+        System.out.printf("Class average: %.2f\n\n", sum / editedMegalist.size());
+
+//        for (int i = 0; i < 7; i++) {
+//            if (names.get(editedMegalist.indexOf(Collections.max(editedMegalist))).equals("Spyros")) {
+//                System.out.printf("%d. %s: \t\t %.2f\n", i + 1,
+//                        names.get(editedMegalist.indexOf(Collections.max(editedMegalist))),
+//                        Collections.max(editedMegalist));
+//                names.remove(editedMegalist.indexOf(Collections.max(editedMegalist)));
+//                editedMegalist.remove(editedMegalist.indexOf(Collections.max(editedMegalist)));
+//            } else {
+//                System.out.printf("%d. %s: \t %.2f\n", i + 1,
+//                        names.get(editedMegalist.indexOf(Collections.max(editedMegalist))),
+//                        Collections.max(editedMegalist));
+//                names.remove(editedMegalist.indexOf(Collections.max(editedMegalist)));
+//                editedMegalist.remove(editedMegalist.indexOf(Collections.max(editedMegalist)));
+//            }
+//        }
+
+        for (int i = 0; i < 7; i++) {
+            System.out.printf("%d. %s: \t %.2f\n", i + 1,
+                    names.get(editedMegalist.indexOf(Collections.max(editedMegalist))),
+                    Collections.max(editedMegalist));
+            names.remove(editedMegalist.indexOf(Collections.max(editedMegalist)));
+            editedMegalist.remove(editedMegalist.indexOf(Collections.max(editedMegalist)));
+
+        }
+        System.out.println();
     }
 }
+
 
 
